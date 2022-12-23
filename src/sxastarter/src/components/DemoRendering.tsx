15bd36d3eaed4a0613be2f237edfb24d8f7cb045
ParvathiSomanhalli <120373557+ParvathiSomanhalli@users.noreply.github.com>
2022-12-23 15:32:54 +0530

    Update DemoRendering.tsx

--- src/sxastarter/src/components/DemoRendering.tsx
+++ src/sxastarter/src/components/DemoRendering.tsx
@@ -14,9 +14,7 @@ export const Default = (props: DemoRenderingProps): JSX.Element => {
   return (
     <div className={`component demorendering ${props.params.styles}`}>
       <div className="component-content">
-    <div>
-    <Text field={props.fields.SomeText}/>
-    </div>
+    <div><Text field={props.fields.SomeText}/></div>
     </div>
     </div>
     );
